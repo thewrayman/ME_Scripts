afc29dec0c45ea8cc83009f80869e9cba757cee2
Emmet Wray <1212344@my.brunel.ac.uk>
2024-03-28 09:23:46 +0000

    GE - Added Selling

--- GrandExchange.lua
+++ GrandExchange.lua
@@ -1,10 +1,16 @@
 --[[
 Author: Tinie
 Inital Release Date: 9th March 2024
-Last Update Data: 23rd March 2024
-Version 2.0
+Last Update Date: 28th March 2024
+Version 2.1.0
 
 --Release Notes
+* 2.1.0
+    Added Selling Abilities
+        top-level usage is the same as buying (example script included at bottom of file)
+        specify an item to sell, with the price and quantity
+    removed some unused code
+
 * 2.0.0
     Major changes
         Reading the Slot data from memory
@@ -25,18 +31,13 @@ local IDS = {
 local VBS = {
     SELECTED_ITEM = 135,
     VOLUME = 136,
-    PRICE = 137
+    PRICE = 137,
+    SELL = 139
 }
 
 local VB_VALUES = {
     CHAT_BOX_OPEN = 11,
     GEWINDOW = 82,
-}
-
-local BASE_INTERFACE = {
-    { 105,1,-1,-1,0 },
-    { 105,3,-1,1,0 },
-    { 105,6,-1,3,0 }
 }
 
 -- GE slot 1-8 interface component ID
@@ -222,6 +223,14 @@ function GE.SearchForItem(itemName, itemId)
     end
 end
 
+function GE.LocateItemInInv(itemId)
+    local inv_container = API.Container_Get_s(93,itemId)
+    if inv_container ~= nil then
+        return inv_container.item_slot
+    end
+    return false
+end
+
 ---Click on the Buy button for the targeted GE slot
 ---@param slot number -- the interface component ID corresponding to the GE slot to target
 ---@return boolean -- successfully opened the Buy window?
@@ -232,6 +241,25 @@ function GE.OpenBuy(slot)
         if API.Compare2874Status(VB_VALUES.CHAT_BOX_OPEN, true) then
             return true
         end
+    end
+    return false
+end
+
+function GE.OpenSell(itemId)
+    if GE.GEWindowOpen() then
+        local itemSlot = GE.LocateItemInInv(itemId)
+        if itemSlot then
+            API.DoAction_Interface(0xffffffff,itemId,1,107,7,itemSlot,3808)
+            API.RandomSleep2(1200,300,600)
+            if API.VB_FindPSettinOrder(VBS.SELECTED_ITEM, 0).state == itemId then
+                API.logDebug("Successfully selected item for selling")
+                return true
+            else
+                API.logError("Tried to select item for selling but looks like the selection failed")
+            end
+        end
+    else
+        API.logError("Opening Sell failed - GE Window not open")
     end
     return false
 end
@@ -346,6 +374,26 @@ function GE.Buy(itemName, itemId, volume, price)
     end
 end
 
+function GE.Sell(itemName, itemId, volume, price)
+    API.logDebug("Tring to sell " .. itemName)
+    local slots = GE.GetAvailableSlots()
+    if #slots == 0 then
+        API.logError("No GE slots available to carry out a sell offer")
+    else
+        if GE.OpenSell(itemId) then
+            if GE.PlaceOrder(volume, price) then
+                GE.ConfirmOrder()
+                API.RandomSleep2(1200,300,600)
+                GE.CollectToInventory()
+                return true
+            else
+                API.logError("Failed to place order, see error stack")
+            end
+        end
+    end
+    return false
+end
+
 function GE.OpenGE()
     if GE.IsInGE() and not GE.GEWindowOpen() then
         API.DoAction_NPC(0x5, API.OFF_ACT_InteractNPC_route, { IDS.CLERK }, 50)
@@ -377,6 +425,49 @@ end
 --                 API.logDebug("Trying to buy ".. i.NAME )
 --                 GE.Buy(i.NAME, i.ID, i.VOL, i.PRICE)
 --             end
+--             API.Write_LoopyLoop(false)
+--         end
+--         API.RandomSleep2(600,600,1200)
+--     else
+--         API.logError("Not in GE - exiting.")
+--         API.Write_LoopyLoop(false)
+--     end
+
+-- end
+--
+-- #############################################################
+-- Example script #2
+
+-- Will take a list of items to buy, and a list of what to sell. Usage for selling is basically identical to buying.
+-- Will go through the buy list first, buy everything it can and then try to sell everything in the sell list
+
+-- local API = require("api")
+-- local GE = require("GrandExchange")
+-- API.SetDrawTrackedSkills(true)
+-- API.SetDrawLogs(true)
+
+-- local itemsToSell = {
+--     {NAME = "Bucket", ID = 1925, VOL = 3, PRICE = 1},
+-- }
+
+-- local itemsToBuy = {
+--     {NAME = "Bucket", ID = 1925, VOL = 3, PRICE = 50000},
+-- }
+
+-- while API.Read_LoopyLoop() do
+--     if GE.IsInGE() then
+--         if GE.OpenGE() then
+--             for _,i in pairs(itemsToBuy) do
+--                 API.logDebug("Trying to buy ".. i.NAME )
+--                 GE.Buy(i.NAME, i.ID, i.VOL, i.PRICE)
+--             end
+
+--             API.RandomSleep2(2000,300,600)
+
+--             for _,i in pairs(itemsToSell) do
+--                 API.logDebug("Trying to sell ".. i.NAME )
+--                 GE.Sell(i.NAME, i.ID, i.VOL, i.PRICE)
+--             end            
 --             API.Write_LoopyLoop(false)
 --         end
 --         API.RandomSleep2(600,600,1200)
