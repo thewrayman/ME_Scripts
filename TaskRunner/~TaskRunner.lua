33577657cc8f3838f2f9aa21d9a2df5d1ca63171
Emmet Wray <1212344@my.brunel.ac.uk>
2024-05-24 18:18:14 +0100

    Update to Runner
    parameterization of focus data. refactor some names

--- TaskRunner/~TaskRunner.lua
+++ TaskRunner/~TaskRunner.lua
@@ -8,24 +8,37 @@ local function drawGUI()
     -- maybe throw some task status stuff here
 end
 
-TASKS = {
-    {SCRIPT = "~TaskLogin", RUNTIME = 60}, -- this would be a file called "~TaskLogin.lua", which would auto-kill if it takes more than 60 seconds to finish
-    {SCRIPT = "~TaskTest2", RUNTIME = 60}, -- this would be a file called "~Task2.lua"
+local exampleOverrideData = {
+    {NAME = "Powder of burials", ID = 52805, VOL = 1, PRICE = 400000},
+    {NAME = "Dinosaur bones", ID = 48075, VOL = 1, PRICE = 50000}
 }
 
-local function getTaskToRun()
-    return TASKS[1]
-end
+-- EXAMPLE TASKS
+-- will use the BuyItems script twice, but with two different sets of data to focus on (one set stored in the BuyItems script itself, another dataset I'm passing in from above)
+TASKS = {
+    {SCRIPT = "~TaskLogin", RUNTIME = 60}, -- this would be a file called "~TaskLogin.lua", which would auto-kill if it takes more than 60 seconds to finish, if in lobby, logs in, exits when in-world
+    {SCRIPT = "~Task_BuyItems", RUNTIME = 600, FOCUS = "prayer"}, -- runs ~BuyItems.lua file, will time out after 10 mins if not already ended, uses optional FOCUS param to let you choose specific features for that execution
+    {SCRIPT = "~Task_BuyItems", RUNTIME = 600, FOCUS = exampleOverrideData} -- FOCUS can be overriden with a specified table if the target task script doesn't contain it already
+}
 
 local function taskRunTime()
     return os.time() - CURRENT_TASK.START
 end
 
 local function setTask()
-    API.logDebug("Loading task " .. getTaskToRun().SCRIPT)
-    CURRENT_TASK = require(getTaskToRun().SCRIPT)
+    API.logDebug("Loading task " .. TASKS[1].SCRIPT)
+    CURRENT_TASK = require(TASKS[1].SCRIPT)
     CURRENT_TASK.START = os.time()
     CURRENT_TASK.COMPLETE = false
+
+    -- optional FOCUS param if wanting to parameterize data being used in that execution of that task
+    if (TASKS[1].FOCUS ~= nil) then
+        if  (type(TASKS[1].FOCUS) == "string") then
+            CURRENT_TASK.setFocus(TASKS[1].FOCUS)
+        elseif (type(TASKS[1].FOCUS) == "table") then
+            CURRENT_TASK.FOCUS = TASKS[1].FOCUS
+        end
+    end
 end
 
 local function finishTask()
@@ -39,6 +52,7 @@ local function finishTask()
     end
 end
 
+--initialise first task to run
 setTask()
 
 --- This while loop will take the place of the while loop in your scripts that this will use.
@@ -47,7 +61,8 @@ while API.Read_LoopyLoop() and (#TASKS > 0) do
     API.DoRandomEvents()
     drawGUI()
 
-    if CURRENT_TASK.COMPLETE or (taskRunTime() > getTaskToRun().RUNTIME)then
+    --has the task set itself as complete or has it reached max time?
+    if CURRENT_TASK.COMPLETE or (taskRunTime() > TASKS[1].RUNTIME)then
         finishTask()
     else
         CURRENT_TASK.run() -- this is basically the equivalent of the main loop
